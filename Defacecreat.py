a1d4f1f2d0e9cea0318c6a19c48d0ef3a8476f96
ajirmdhn <45916317+ajirmdhn@users.noreply.github.com>
2019-06-06 11:46:29 +0700

    Update Defacecreat.py

--- Defacecreat.py
+++ Defacecreat.py
@@ -4,11 +4,11 @@
 #######################
 mess = """======================================================
              Deface Script Creator        
-                     Muhammad Fazriansyah
+              Muhammad Fazriansyah
 ======================================================"""
 
 print mess
-print "Created by Ubaii ID"
+print "Created by Muhammad Fazriansyah"
 title = raw_input("Judul title: ")
 heading = raw_input("Hacked by: ")
 imagelink = raw_input("link gambar (tengah): ")
